eb2b9653da9afc5acb988e081ea4761df3e42f6f
eleensmathew <eleensmathew@gmail.com>
2024-10-02 12:28:38 +0530

    added tests for compare_and_replace

--- tests/allFileMetadata.py
+++ tests/allFileMetadata.py
@@ -1,14 +1,6 @@
 import pytest
 from twinTrim.dataStructures import allFileMetadata
 
-def test_add_or_update_file_when_file_doesnt_exist(mocker):
-    mockfile = "mockfile.txt"
-    mocker.patch("twinTrim.dataStructures.allFileMetadata.os.path.exists", return_value=False)
-    mocker.patch("twinTrim.dataStructures.allFileMetadata.get_file_hash", side_effect=FileNotFoundError)
-    
-    with pytest.raises(FileNotFoundError):
-        allFileMetadata.add_or_update_file(mockfile)
-
 def test_compare_and_replace_new_file_is_newer(mocker):
 
     file1 = "file1.txt"
