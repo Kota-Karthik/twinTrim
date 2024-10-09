9f1183f3ffc4803587e1b29e6fedb65f40e48d72
Sai-ganesh-0004 <322103310216@gvpce.ac.in>
2024-10-09 09:01:22 +0530

    updated the path

--- tests/test_fileMetadata.py
+++ tests/test_fileMetadata.py
@@ -5,7 +5,7 @@ from twinTrim.dataStructures.fileMetadata import FileMetadata
 def test_insert_new_file():
     # Test inserting a new file into the metadata
     metadata = FileMetadata([])
-    file_path = "/path/to/file1.txt"
+    file_path = "C:\\Users\\2004s\\Desktop\\dummy\\dummy_1.txt"
     
     metadata.insert_file(file_path)
     
@@ -14,7 +14,7 @@ def test_insert_new_file():
 
 def test_insert_duplicate_file():
     # Test trying to insert a file that is already present
-    file_path = "/path/to/file2.txt"
+    file_path = "C:\\Users\\2004s\\Desktop\\dummy\\dummy_2.txt"
     metadata = FileMetadata([file_path])
     
     metadata.insert_file(file_path)
@@ -25,8 +25,8 @@ def test_insert_duplicate_file():
 
 def test_insert_multiple_files():
     # Test inserting multiple different files
-    file_path1 = "/path/to/file3.txt"
-    file_path2 = "/path/to/file4.txt"
+    file_path1 = "C:\\Users\\2004s\\Desktop\\dummy\\dummy_3.txt"
+    file_path2 = "C:\\Users\\2004s\\Desktop\\dummy\\dummy_4.txt"
     
     metadata = FileMetadata([file_path1])
     
